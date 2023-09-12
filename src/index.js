dc427b0b579b27ebb7f2c9ced051733426716297
VinothKumar-Vu <vinothkumar02141@gmail.com>
2023-09-12 11:59:11 +0530

    modificatation in index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const cors = require('cors')
+App.arguments(cors('*'))
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
